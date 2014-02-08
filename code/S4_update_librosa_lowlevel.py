ce82ef70a88acd79d4a4a69a25540ffe0e123308
Brian McFee <brm2132@columbia.edu>
2014-02-08 13:31:30 -0500

    refactored lowlevel update; resolves #10

--- code/S4_update_librosa_lowlevel.py
+++ code/S4_update_librosa_lowlevel.py
@@ -3,221 +3,19 @@
 # CREATED:2014-01-02 13:07:49 by Brian McFee <brm2132@columbia.edu>
 # revise/recompute existing feature analysis 
 
-import numpy as np
-import scipy.signal
-import librosa
-import mutagen
-import os
-import git
-
-import json
-import cPickle as pickle
-
-from joblib import Parallel, delayed
-
-import sys
 import argparse
+import cPickle as pickle
+import json
+import os
+import sys
+from joblib import Parallel, delayed
 
 import gordon
 
-__version__     = '0.1-dev'
-ANALYSIS_NAME   = 'librosa:low-level'
+# Pull in the audio analyzer
+from seymour_analyzers import librosa_lowlevel
 
-def get_git_revision(filename):
-    '''Walk up a path name until we find a git dir, then grab its current revision.
-    
-    If the file is not under any git repository, returns ''.
-    '''
-    
-    filename = os.path.realpath(filename)
-
-    while filename != '/':
-        try:
-            g = git.Git(filename)
-            rev = g.log(['-n 1', '--format=%H'])
-            return rev
-        except:
-            filename = os.path.dirname(filename)
-    return ''
-
-# Environment settings
-ENVIRONMENT = {'librosa':    {'version':      librosa.__version__,
-                              'git-revision': get_git_revision(librosa.__file__),
-                              'timestamp':    os.path.getmtime(librosa.__file__)
-                              },
-                'analyzer':  {'version':        __version__,
-                              'git-revision':   get_git_revision(__file__),
-                              'timestamp':      os.path.getmtime(__file__)
-                },
-               }
-
-#-- Audio analysis guts
-def analyze_hpss(y, PARAMETERS):
-    '''Separate harmonic and percussive audio time series'''   
-    # Get the STFT
-    D = librosa.stft(y, **PARAMETERS['stft'])
-    
-    
-    # Get the HPSS
-    D_h, D_p = librosa.decompose.hpss(D, **PARAMETERS['hpss'])
-
-    y_h = librosa.istft(D_h, hop_length=PARAMETERS['stft']['hop_length'])
-    y_p = librosa.istft(D_p, hop_length=PARAMETERS['stft']['hop_length'])
-    
-    return y_h, y_p
-
-def analyze_melspec(y, PARAMETERS):
-    '''Get a mel power spectrogram'''   
-    S = librosa.feature.melspectrogram(y=y, 
-                                       sr=PARAMETERS['load']['sr'], 
-                                       n_fft=PARAMETERS['stft']['n_fft'], 
-                                       hop_length=PARAMETERS['stft']['hop_length'], 
-                                       n_mels=PARAMETERS['mel']['n_mels'], 
-                                       fmax=PARAMETERS['mel']['fmax']).astype(np.float32)
-    
-    return S
-    
-def analyze_mfcc(S, PARAMETERS):
-    '''Get MFCCs from the mel power spectrogram'''
-    M = librosa.feature.mfcc(S=librosa.logamplitude(S, ref_power=np.abs(S).max()), 
-                             n_mfcc=PARAMETERS['mfcc']['n_mfcc'])
-    return M
-
-def analyze_tuning(y, PARAMETERS):
-    '''Estimate tuning'''
-    pitches, magnitudes = librosa.feature.ifptrack(y, 
-                                                   sr=PARAMETERS['load']['sr'],
-                                                   n_fft=PARAMETERS['stft']['n_fft'],
-                                                   hop_length=PARAMETERS['stft']['hop_length'])[:2]
-    pitches = pitches[magnitudes > np.median(magnitudes)]
-    
-    tuning = librosa.feature.estimate_tuning(pitches)
-    
-    return tuning
-
-def analyze_beat(y, PARAMETERS):
-    '''Estimate beat times and tempo'''
-    # Compute a log-power mel spectrogram on the percussive component
-    S_p = librosa.feature.melspectrogram(y=y, 
-                                         sr=PARAMETERS['load']['sr'], 
-                                         n_fft=PARAMETERS['stft']['n_fft'], 
-                                         hop_length=PARAMETERS['beat']['hop_length'],
-                                         n_mels=PARAMETERS['mel']['n_mels'],
-                                         fmax=PARAMETERS['mel']['fmax'])
-    
-    S_p = librosa.logamplitude(S_p, ref_power=S_p.max())
-    
-    # Compute the median onset aggregation
-    odf = librosa.onset.onset_strength(S=S_p, aggregate=np.median)
-    
-    # Get beats
-    tempo, beats = librosa.beat.beat_track(onsets=odf, 
-                                           sr=PARAMETERS['load']['sr'], 
-                                           hop_length=PARAMETERS['beat']['hop_length'], 
-                                           n_fft=PARAMETERS['stft']['n_fft'])
-      
-    beat_times = librosa.frames_to_time(beats, 
-                                        sr=PARAMETERS['load']['sr'], 
-                                        hop_length=PARAMETERS['beat']['hop_length'])
-    
-    return tempo, beat_times, odf
-    
-def analyze_chroma(y, PARAMETERS):
-    '''STFT-chromagram'''
-    C = librosa.feature.chromagram(y=y, 
-                                   sr=PARAMETERS['load']['sr'], 
-                                   hop_length=PARAMETERS['stft']['hop_length'], 
-                                   **PARAMETERS['chroma']).astype(np.float32)
-    
-    return C
-    
-def analyze_cqt(y, PARAMETERS):
-    '''Constant-Q transform'''
-    CQT = librosa.cqt(y, 
-                      PARAMETERS['load']['sr'],
-                      hop_length=PARAMETERS['stft']['hop_length'], 
-                      **PARAMETERS['cqt']).astype(np.float32)
-    
-    return CQT
-
-def analyze_audio(input_file, features, analysis=None, PARAMETERS=None):
-    '''Full audio analysis'''
-    if analysis is None:
-        analysis = {}
-    
-    # Import metadata, if we can
-    try:
-        analysis['metadata'] = dict(mutagen.File(input_file, easy=True))
-    except:
-        analysis['metadata'] = {}
-    
-    
-    # Load the input file
-    y, sr = librosa.load(input_file, **PARAMETERS['load'])
-    
-    # Compute track duration
-    if 'duration' in features:
-        analysis['duration'] = float(len(y)) / sr
-    
-    # Pre-compute a downsampled time series for vis purposes
-    if 'signal' in features:
-        analysis['signal']   = scipy.signal.decimate(y, 
-                                                    len(y)/1024, 
-                                                    ftype='fir').astype(np.float32)
-    
-    # Get a mel power spectrogram
-    if 'mel_spectrogram' in features:
-        analysis['mel_spectrogram'] = analyze_melspec(y, PARAMETERS)
-    
-    # Get the mfcc's
-    if 'mfcc' in features or 'mel_spectrogram' in features:
-        analysis['mfcc'] = analyze_mfcc(analysis['mel_spectrogram'], PARAMETERS)
-
-    # Do HPSS
-    y_h, y_p = analyze_hpss(y, PARAMETERS)
-    
-    # We're done with raw audio
-    del y
-    
-    # Get beats and tempo
-    if 'beats' in features:
-        analysis['tempo'], analysis['beat_times'], analysis['onset_strength'] = analyze_beat(y_p, PARAMETERS)
-    
-    # We're done with percussion now
-    del y_p
-    
-    # Get tuning
-    if 'tuning' in features:
-        analysis['tuning'] = analyze_tuning(y_h, PARAMETERS)
-    
-    # Get the chroma
-    if 'chroma' in features:
-        analysis['chroma'] = analyze_chroma(y_h, PARAMETERS)
-    
-    # Get the CQT
-    if 'cqt' in features:
-        analysis['cqt']    = analyze_cqt(y_h, PARAMETERS)
-    
-    # We're done with harmonics now
-    if 'PREVIOUS' in analysis:
-        PREV = analysis['PREVIOUS']
-    else:
-        PREV = {}
-
-    if 'updated_features' in analysis:
-        PREV['updated_features'] = features
-
-
-    analysis['updated_features'] = features
-
-    analysis['PREVIOUS'] = {'PARAMETERS':   analysis['PARAMETERS'], 
-                            'ENVIRONMENT':  analysis['ENVIRONMENT'],
-                            'PREVIOUS':     PREV}
-
-    analysis['PARAMETERS'] = PARAMETERS
-    analysis['ENVIRONMENT'] = ENVIRONMENT
-
-    return analysis
+ANALYSIS_NAME   = librosa_lowlevel.__description__
 
 #-- interface guts
 def get_output_file(path, ext='pickle'):
@@ -305,7 +103,10 @@ def update_annotation(track_id, feature_directory, features, PARAMETERS):
     print 'Updating ', track.path
     
     analysis = load_analysis(output_file)
-    analysis = analyze_audio(track.fn_audio, set(features), analysis=analysis, PARAMETERS=PARAMETERS)
+    analysis = librosa_lowlevel.analyze_audio(track.fn_audio, 
+                                              features=set(features), 
+                                              analysis=analysis, 
+                                              PARAMETERS=PARAMETERS)
 
     filename = save_analysis(track.path, analysis, feature_directory)
     return track_id, filename
