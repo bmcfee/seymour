1a27d93ab73b9a0076b5b320755753893ec7ac34
Brian McFee <brm2132@columbia.edu>
2014-02-11 13:42:44 -0500

    seems to work!

--- learning/learn_segmenter.py
+++ learning/learn_segmenter.py
@@ -7,7 +7,6 @@ import sys
 from joblib import Parallel, delayed
 import mir_eval
 import numpy as np
-import cPickle as pickle
 
 import OLDA
 import seymour
@@ -84,6 +83,7 @@ def get_track_data(t_id):
     return {'features': features,
             'k_min':    k_min,
             'k_max':    k_max,
+            'beat_times':     analysis_mid['beat_times'],
             'boundary_times': boundary_times,
             'boundary_beats': boundary_beats}
     
@@ -101,6 +101,69 @@ def get_training_data(collection_names):
 
     return data
 
+def score_example(W, example):
+    '''Compute the NCE F-measure score for an example'''
+
+    seg_predict_tree, best_idx = midlevel.get_segments(W.dot(example['features']),
+                                                       example['k_min'],
+                                                       example['k_max'])
+
+    # Make sure the beat times cover the entire range, pad
+    beat_times      = mir_eval.util.adjust_times(example['beat_times'], 
+                                                 t_min=0.0,
+                                                 t_max=example['boundary_times'][-1])[0]
+
+    predicted_times = beat_times[seg_predict_tree[best_idx]]
+
+    # Tack on the track duration to the end
+    predicted_times = mir_eval.util.adjust_times(predicted_times, 
+                                                 t_min=0.0,
+                                                 t_max=example['boundary_times'][-1])[0]
+
+    # Compute the score, pull off the f-measure
+    score = mir_eval.segment.frame_clustering_nce(example['boundary_times'], predicted_times)[-1]
+    return score
+
+def fit_model(train_data, num_jobs=1, sigma_min=0, sigma_max=9):
+    '''Learning code for segmentation'''
+
+    # Stack the features into a list
+    features            = []
+    boundary_beats      = []
+
+    for t_id in train_data:
+        features.append(train_data[t_id]['features'])
+        boundary_beats.append(train_data[t_id]['boundary_beats'])
+
+    # Parameter sweep range
+    sigma_values = 10.0**np.arange(sigma_min, sigma_max+1)
+
+    best_score  =   -np.inf
+    best_sigma  =   None
+    best_model  =   None
+
+    for sigma in sigma_values:
+        # Construct the model
+        olda_model = OLDA.OLDA(sigma=sigma)
+
+        # Fit the model
+        olda_model.fit(features, boundary_beats)
+
+        # Evaluate on the training set
+        scores = Parallel(n_jobs=num_jobs)(delayed(score_example)(olda_model.components_, example) 
+                                            for _, example in train_data.iteritems())
+
+        mean_score = np.mean(scores)
+        print '\tsigma=%.2e, score=%.3f' % (sigma, mean_score)
+
+        if mean_score > best_score:
+            best_score  = mean_score
+            best_sigma  = sigma
+            best_model  = olda_model.components_
+
+    print 'Best sigma: %.2e' % best_sigma
+    return best_model
+
 def learn_segmenter(model_file=None, num_jobs=1, collections=None, sigma_min=0, sigma_max=9):
     '''Outer layer of the segment learner'''
 
@@ -110,11 +173,12 @@ def learn_segmenter(model_file=None, num_jobs=1, collections=None, sigma_min=0, 
 
     # Step 2: learn the model
     print 'Fitting the model... '
+    model   =   fit_model(train_data, num_jobs=num_jobs, sigma_min=sigma_min, sigma_max=sigma_max)
 
     # Step 3: save the results
     print 'Saving to ', model_file
+    np.save(model_file, model)
 
-    pass
 
 if __name__ == '__main__':
     parameters = process_arguments(sys.argv[1:])
